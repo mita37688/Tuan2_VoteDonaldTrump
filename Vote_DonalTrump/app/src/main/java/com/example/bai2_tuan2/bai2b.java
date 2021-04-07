79d4c14eaa6bb88328d722951abac674fbf4b1be
Tr Th Minh Tân <mita37688>
2021-04-07 20:33:14 +0700

    BaiTapTHTuan2_VoteDonaldTrump

---
+++ Vote_DonalTrump/app/src/main/java/com/example/bai2_tuan2/bai2b.java
@@ -0,0 +1,14 @@
+package com.example.bai2_tuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bai2b extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bai2b);
+    }
+}
---
+++ Vote_DonalTrump/app/src/main/java/com/example/bai2_tuan2/bai2c.java
@@ -0,0 +1,14 @@
+package com.example.bai2_tuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bai2c extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bai2c);
+    }
+}
